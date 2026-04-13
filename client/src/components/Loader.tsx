eb2fab02e89691297e0da1d216373e125e2acb8f
yvi-opash <yvi.opash@gmail.com>
2026-04-13 11:39:46 +0530

    okay

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -10,35 +10,22 @@ const Loading: React.FC<LoadingProps> = ({
   message = 'Loading', 
   fullScreen = false
 }) => {
-  // Split message into characters for animation
-  const characters = message.split('');
+
 
   return (
     <div className={`loading-container ${fullScreen ? 'loading-fullscreen' : ''}`}>
-      <div className="text-animation">
-        {characters.map((char, index) => (
-          <span 
-            key={index} 
-            className="animated-char"
-            style={{
-              animationDelay: `${index * 0.08}s`
-            }}
-          >
-            {char === ' ' ? '\u00A0' : char}
-          </span>
-        ))}
-      </div>
       
+      
+
+      <div className="cricket-info">
+        <span className="loading-subtext">CricIt</span>
+        <span className="ball-icon">🏏</span>
+      </div>
+
       <div className="cricket-dots">
         <div className="dot"></div>
         <div className="dot"></div>
         <div className="dot"></div>
-      </div>
-
-      <div className="cricket-info">
-        <span className="ball-icon">🏏</span>
-        <span className="loading-subtext">CricIt</span>
-        <span className="ball-icon">🏏</span>
       </div>
     </div>
   );
